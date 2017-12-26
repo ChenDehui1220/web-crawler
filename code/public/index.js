497cc1c7f1195995b15e5b43811c24eacf8cb570
gary_chen <gary_chen@hiiir.com>
2017-12-26 14:41:46 +0800

    add range slider

--- code/public/index.js
+++ code/public/index.js
@@ -1,26 +1,54 @@
 (function() {
     var gData = {};
     var sortPrice = 'low'; // low or high
+    var $Slider = $('#slider-range');
+    var rangeMin = 0;
+    var rangeMax = 50000;
+    var rangeTop = 100000;
 
     var parse = function() {
         var data = gData;
-        var output = '<table class="table table-striped"> <thead> <tr> <th>Product</th> <th class="sortPrice">Price</th> <th>Shop</th> <th>Platform</th> </tr></thead> <tbody>';
+        var range = $Slider.slider('values');
+        var min = range[0];
+        var max = range[1];
+        var output =
+            '<table class="table table-striped"> <thead> <tr> <th>Product</th> <th class="sortPrice">Price</th> <th>Shop</th> <th>Platform</th> </tr></thead> <tbody>';
+        var priceAry = [], dataprice = 0;
 
-        for(var i in data) {
-            output += '<tr> <td>'+data[i].name+'</td><td>'+data[i].price+'</td><td>'+data[i].shop+'</td><td>'+data[i].platform+'</td></tr>';
+        for (var i in data) {
+            priceAry = data[i].price.split('~');
+            dataprice = parseInt(priceAry[0].replace(/(\s|,)/g,''))
+
+            if (min <= dataprice && dataprice <= max) {
+                output +=
+                    '<tr> <td>' +
+                    data[i].name +
+                    '</td><td>' +
+                    data[i].price +
+                    '</td><td>' +
+                    data[i].shop +
+                    '</td><td>' +
+                    data[i].platform +
+                    '</td></tr>';
+            }
         }
 
         output += '</tbody> </table>';
         $('#results').html(output);
-    }
+    };
 
     var fetch = function() {
-        var ky = $('#keyword').val();
+        var $dom = $('#keyword');
+        var ky = $dom.val();
         var results = $('#results');
 
-        if (ky === '') {results.html(''); return false;}
+        if (ky === '') {
+            results.html('');
+            $dom.focus();
+            return false;
+        }
 
-        $.getJSON('/query?keyword=' + ky, function(obj){
+        $.getJSON('/query?keyword=' + ky, function(obj) {
             gData = obj.data;
             parse();
 
@@ -35,15 +63,22 @@
             e.preventDefault();
             fetch();
         });
-        // $('#keyword').on('keyup', function(){
-        //     var val = $(this).val();
-        //     if (val !== '' && val.length > 4) {
-        //         fetch();
-        //     }
-        // });
         $(document).on('click', '.sortPrice', function() {
             gData = Object.assign([], gData).reverse();
             parse();
         });
+
+        $Slider.slider({
+            range: true,
+            min: rangeMin,
+            max: rangeTop,
+            values: [rangeMin, rangeMax],
+            slide: function(event, ui) {
+                $('#amount').val('$' + ui.values[0] + ' - $' + ui.values[1]);
+            }
+        });
+        $('#amount').val(
+            '$' + $Slider.slider('values', 0) + ' - $' + $Slider.slider('values', 1)
+        );
     });
 })();
